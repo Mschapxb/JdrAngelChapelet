c17be3c1a5f935854c9e6fa040eedd640f84eb5f
Mschap <65339152+Mschapxb@users.noreply.github.com>
2023-08-01 13:14:00 +0200

    version 1

---
+++ src/test/java/com/example/fantasyrpg/FantasyrpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fantasyrpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyrpgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
